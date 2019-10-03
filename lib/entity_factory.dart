8673db968cc20e883582eaced3c42e16ba53a034
tushar <tushar.paliwal1997@gmail.com>
2019-10-03 05:48:07 +0530

    add EntityFactory

---
+++ lib/entity_factory.dart
@@ -0,0 +1,16 @@
+import 'package:flutter_provider_app/models/postsfeed/posts_feed_entity.dart';
+import 'package:flutter_provider_app/models/subreddit_info/subreddit_information_entity.dart';
+
+class EntityFactory {
+  static T generateOBJ<T>(json) {
+    if (1 == 0) {
+      return null;
+    } else if (T.toString() == "PostsFeedEntity") {
+      return PostsFeedEntity.fromJson(json) as T;
+    } else if (T.toString() == "SubredditInformationEntity") {
+      return SubredditInformationEntity.fromJson(json) as T;
+    } else {
+      return null;
+    }
+  }
+}
